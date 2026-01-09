bbb91660c84f3de419afc79ad71491449f9bcff3
undead-undead <244278928+undead-undead@users.noreply.github.com>
2026-01-09 20:40:46 +0800

    V18: Added HTTP probe detection logging to debug 414-byte mystery

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 # Version / 版本
-VERSION="v0.2.17-timeout"
+VERSION="v0.2.18-debug"
 REPO="undead-undead/xray-lite"
 
 echo -e "${BLUE}=========================================${NC}"
