0f95ab80e90181a6683934960b2598c6351a9067
undead-undead <244278928+undead-undead@users.noreply.github.com>
2026-01-13 21:42:15 +0800

    UX: Optimized installation scripts and cleaned up reality logs

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 # Version / 版本
-VERSION="v0.3.2"
+VERSION="v0.3.3"
 REPO="undead-undead/xray-lite"
 
 echo -e "${BLUE}=========================================${NC}"
